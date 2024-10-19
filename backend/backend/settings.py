bb53ed2e16cf22b5eefcb0ed2a4cb267ceb44c45
sxdisbr <yc-user@epddj8o8echpd6072r4f.auto.internal>
2024-10-19 17:23:58 +0000

    Initial commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['158.160.8.48', '127.0.0.1', 'localhost', 'taskisx.zapto.org']
 
 
 # Application definition
@@ -103,7 +103,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_backend/'
+
+STATIC_ROOT = BASE_DIR / 'static_backend'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
---
+++ backend/settings.py
@@ -0,0 +1,8 @@
+ALLOWED_HOSTS = ['158.160.8.48', '127.0.0.1', 'localhost'] 
+STATIC_URL = '/static_backend/'
+STATIC_ROOT = BASE_DIR / 'static_backend'
+
+
+
+
+ 
